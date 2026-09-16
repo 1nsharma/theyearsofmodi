2ca50297fe1e431e0fae01dc278a81850183bbd7
Amit Sharma <amitsharmaanxt@gmail.com>
2026-09-17 01:55:25 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 3000
+  },
+  build: {
+    target: 'esnext',
+    minify: 'esbuild',
+    sourcemap: false
+  },
+  optimizeDeps: {
+    include: ['three', '@react-three/fiber', '@react-three/drei', 'gsap']
+  }
+})
